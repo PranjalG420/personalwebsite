c3449e2e0f805a67022693789001b5f1a93c98d0
Pranjal Gupta <95481637+PranjalG420@users.noreply.github.com>
2023-02-02 21:06:13 +0530

    style: removed styling from _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,13 +15,7 @@ export default function MyApp({
                 <link rel="icon" href="/pglogo.png" />
             </Head>
             <ThemeProvider attribute="class">
-                <div className="min-h-screen flex flex-col items-center text-black dark:text-white dark:bg-zinc-900 ">
-                    <Navbar />
-                    <div className="flex flex-col flex-1 items-center tablet:max-w-[900px]">
-                        <Component {...pageProps} />
-                    </div>
-                    <Footer />
-                </div>
+                <Component {...pageProps} />
             </ThemeProvider>
         </SessionProvider>
     );
