3bc01623b4820b90205bdcaf771329bc4870fe9c
Pranjal Gupta <95481637+PranjalG420@users.noreply.github.com>
2023-02-02 20:59:03 +0530

    fix: updated container to center content on page

--- components/container.tsx
+++ components/container.tsx
@@ -1,14 +1,13 @@
 import React, { PropsWithChildren } from "react";
+import Navbar from "./navbar";
 
-export default function Container({ children, top }) {
+export default function Container({ children }) {
     return (
-        <main
-            className={
-                `flex flex-col justify-around items-start w-screen px-4 tablet:max-w-[900px] ` +
-                top
-            }
-        >
-            {children}
-        </main>
+        <div className="flex flex-col w-full justify-start h-full min-h-screen text-white bg-black">
+            <Navbar />
+            <main className="flex flex-col mx-auto h-full justify-center flex-1 max-w-[1200px] px-4">
+                {children}
+            </main>
+        </div>
     );
 }
