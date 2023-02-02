3fcc470f41246d3e7048ab38f6ff5a86ea25ff3e
Pranjal Gupta <95481637+PranjalG420@users.noreply.github.com>
2023-02-02 21:04:21 +0530

    fix: deleted markdown

--- lib/markdownToHtml.ts
+++
@@ -1,8 +0,0 @@
-import { remark } from "remark";
-import html from "remark-html";
-import gfm from "remark-gfm";
-
-export default async function markdownToHtml(markdown: string) {
-    const result = await remark().use(html).use(gfm).process(markdown);
-    return result.toString();
-}
