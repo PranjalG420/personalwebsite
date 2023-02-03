f804d7c58b8288ef480fb55f9b5ab1b5c3b8dbde
Pranjal Gupta <95481637+PranjalG420@users.noreply.github.com>
2023-02-03 12:55:37 +0530

    style: text/bg color, font-mono

--- components/container.tsx
+++ components/container.tsx
@@ -3,9 +3,9 @@ import Navbar from "./navbar";
 
 export default function Container({ children }) {
     return (
-        <div className="flex flex-col w-full justify-start h-full min-h-screen text-white bg-black">
+        <div className="flex flex-col w-full justify-start h-full min-h-screen text-white bg-black font-mono">
             <Navbar />
-            <main className="flex flex-col mx-auto h-full justify-center flex-1 max-w-[1200px] px-4">
+            <main className="flex flex-col mx-auto h-full justify-center flex-1 max-w-[72rem] px-4">
                 {children}
             </main>
         </div>
