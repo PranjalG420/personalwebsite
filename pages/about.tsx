3c194400998482033971c9f1727fd3bf5c7012dd
Pranjal Gupta <95481637+PranjalG420@users.noreply.github.com>
2023-02-05 22:43:40 +0530

    feat: animated signature

--- pages/about.tsx
+++ pages/about.tsx
@@ -11,24 +11,47 @@ export default function Projects() {
             <Head>
                 <title>About</title>
             </Head>
-            <h1 className="desktop:text-4xl text-2xl font-semibold mb-2">
-                Who am I?
-            </h1>
-            <p className="desktop:text-xl text-base">
-                My name is Pranjal Gupta. I'm an aspiring web developer who is
-                constantly looking for new challenges and opportunities to test
-                and expand my skillset. I am passionate about creating
-                high-quality, user-friendly websites and applications that are
-                highly functional. If you are looking for a dedicated and
-                reliable developer, look no further!
-            </p>
+            <div className="flex flex-row items-center justify-center">
+                <div className="flex flex-col">
+                    <h1 className="desktop:text-4xl text-2xl font-semibold mb-2">
+                        Who am I?
+                    </h1>
+                    <p className="desktop:text-xl text-base">
+                        My name is Pranjal Gupta. I'm an aspiring web developer
+                        who is constantly looking for new challenges and
+                        opportunities to test and expand my skillset. I am
+                        passionate about creating high-quality, user-friendly
+                        websites and applications that are highly functional. If
+                        you are looking for a dedicated and reliable developer,
+                        look no further!
+                    </p>
+                </div>
+                <div className="flex flex-row items-center justify-center gap-2">
+                    <svg
+                        width="209"
+                        height="98"
+                        viewBox="0 0 209 98"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path
+                            id="signature"
+                            d="M38 9C33.7673 27.8183 29.3866 46.1857 21.7778 64C18.5444 71.5702 12.3787 87.5711 4.22222 92.5556C-0.792464 95.6201 3.62493 80.6446 5.05555 74.9444C10.7724 52.1662 18.2519 24.9495 35.2778 7.72222C49.3612 -6.52785 65.2546 6.53353 54.3333 21.7778C48.4803 29.9476 39.1744 34.0478 32.2222 41C30.9982 42.224 23.2234 50.5524 29.1111 48.1111C42.9115 42.389 56.6052 40.2222 71.6111 40.2222C89.011 40.2222 106.299 44.6354 123.667 43.7778C132.797 43.3269 136.354 38.9734 141.5 31.9444C144.638 27.6588 147.483 23.145 150.333 18.6667C151.366 17.0443 140.95 17.5546 140 17.8889C128.309 22.0023 118.043 35.8874 111 45.2778C99.9998 59.9447 90.6263 75.3813 76.4444 87.3333C71.9159 91.1498 65.2992 96.9563 58.7222 95.9444C55.3612 95.4274 56.862 83.4653 57.6667 81.6667C63.935 67.6551 95.1566 71.1583 106.667 71.0556C140.109 70.757 173.562 71.5419 207 72"
+                            stroke="white"
+                            strokeWidth="3"
+                            strokeLinecap="round"
+                        />
+                    </svg>
+                </div>
+            </div>
+
             {/* <h1 className="desktop:text-4xl text-2xl font-semibold mt-16">
                 My Skills
             </h1>
             <div className="flex flex-row flex-wrap">
 
             </div> */}
-            <h1 className="desktop:text-4xl text-2xl font-semibold desktop:mt-16 mt-8">
+            <h1 className="desktop:text-4xl text-2xl font-semibold mt-8">
                 My Projects
             </h1>
             <div className="flex flex-row flex-wrap justify-around items-center mt-4 border-t-2">
