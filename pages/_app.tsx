99cce9c2cabc0d52d4c75749c9c57a20eb0c3859
Pranjal Gupta <95481637+PranjalG420@users.noreply.github.com>
2023-02-05 22:43:51 +0530

    feat: removed footer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,4 @@
 import "../styles/globals.css";
-import Navbar from "../components/navbar";
-import Footer from "../components/footer";
 import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "next-themes";
 import Head from "next/head";
