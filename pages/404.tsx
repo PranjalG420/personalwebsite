402ad9d9914f9f23303907fe90620f0a6044a0de
Pranjal Gupta <95481637+PranjalG420@users.noreply.github.com>
2023-02-05 13:37:22 +0530

    style: font size

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,7 +1,6 @@
 import Head from "next/head";
 import React from "react";
 import Container from "../components/container";
-import Link from "next/link";
 import { CustomLink } from "../components/smallcomponents";
 
 export default function errorPage() {
@@ -11,10 +10,10 @@ export default function errorPage() {
                 <title>404</title>
             </Head>
             <div className="flex flex-col items-center justify-around mx-auto">
-                <p className="text-3xl tablet:text-5xl font-semibold">
+                <p className="text-3xl desktop:text-4xl font-semibold">
                     404 Not Found
                 </p>
-                <p className="text-base tablet:text-lg my-4 italic text-zinc-600">
+                <p className="text-base tablet:text-lg my-4 italic text-neutral-400">
                     It seems what you are trying to find does not exist. Perhaps
                     you entered something wrong.
                 </p>
