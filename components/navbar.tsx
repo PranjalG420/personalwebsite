774c60281dc19d1a0d4bff2c483422e2689f0736
Pranjal Gupta <95481637+PranjalG420@users.noreply.github.com>
2023-02-05 13:38:17 +0530

    fix: changed link from projects to about

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,14 +2,14 @@ import Link from "next/link";
 
 export default function navbar() {
     return (
-        <nav className="flex flex-row sticky top-0 w-full desktop:justify-start justify-between desktop:text-xl text-base backdrop-blur print:hidden">
+        <nav className="flex flex-row sticky top-0 w-full justify-start desktop:text-xl text-base backdrop-blur print:hidden">
             <div className="flex items-center flex-row">
                 <Link href="/">
                     <p className="px-4 py-2 hover:cursor-pointer hover:bg-neutral-800">
                         home
                     </p>
                 </Link>
-                <Link href="/projects">
+                <Link href="/about">
                     <p className="px-4 py-2 hover:cursor-pointer hover:bg-neutral-800">
                         about
                     </p>
