2d50b438257c145460bd63f9eefc4a3754125670
Pranjal Gupta <95481637+PranjalG420@users.noreply.github.com>
2023-02-05 22:21:32 +0530

    fix: removed search bar

--- pages/about.tsx
+++ pages/about.tsx
@@ -3,17 +3,13 @@ import { useState } from "react";
 import Container from "../components/container";
 import projectData from "../components/projectData";
 import { ChevronLeft, ChevronRight } from "react-feather";
+import Image from "next/image";
 
 export default function Projects() {
-    const [search, setSearch] = useState<string>("");
-    const maxID = projectData
-        .map((item) => item.id)
-        .reduce((acc) => acc + 1, 0);
-
     return (
         <Container>
             <Head>
-                <title>Projects</title>
+                <title>About</title>
             </Head>
             <h1 className="desktop:text-4xl text-2xl font-semibold mb-2">
                 Who am I?
@@ -32,35 +28,16 @@ export default function Projects() {
             <div className="flex flex-row flex-wrap">
 
             </div> */}
-            <h1 className="desktop:text-4xl text-2xl font-semibold mt-16">
+            <h1 className="desktop:text-4xl text-2xl font-semibold desktop:mt-16 mt-8">
                 My Projects
             </h1>
-            <input
-                type="search"
-                spellCheck="false"
-                className="bg-neutral-800 p-1 text-neutral-400 focus:outline-none rounded mt-4"
-                placeholder={
-                    "Search through " +
-                    maxID +
-                    (maxID == 1 ? " project" : " projects")
-                }
-                onChange={(e) => {
-                    setSearch(e.target.value);
-                }}
-            ></input>
             <div className="flex flex-row flex-wrap justify-around items-center mt-4 border-t-2">
-                {" "}
                 {projectData.map((item) => {
-                    if (
-                        !item.title.toLowerCase().includes(search.toLowerCase())
-                    ) {
-                        return;
-                    }
                     const [slide, setSlide] = useState<number>(0);
                     return (
                         <div
                             key={item.id}
-                            className="flex desktop:flex-row flex-col items-start gap-2 border-b-2 w-full py-4"
+                            className="flex desktop:flex-row flex-col items-start gap-2 border-b-2 py-6"
                         >
                             <div className="relative">
                                 {/* Left carousel button */}
@@ -93,6 +70,11 @@ export default function Projects() {
                                         "desktop:max-w-[34rem] w-[34rem] rounded "
                                     }
                                 />
+                                {/* <Image
+                                    src={item.images[slide]}
+                                    alt="image not found"
+                                    fill={true}
+                                /> */}
 
                                 {/* Right carousel button */}
 
@@ -120,12 +102,12 @@ export default function Projects() {
                                     </h1>
                                     <p>{item.text}</p>
                                 </div>
-                                <div className="flex flex-col">
+                                <div className="flex flex-col items-start">
                                     <a
                                         href={item.gitLink}
                                         target="_blank"
                                         rel="noopener noreferrer"
-                                        className="text-blue-500"
+                                        className="text-blue-500 hover:underline"
                                     >
                                         GitHub
                                     </a>
@@ -133,7 +115,7 @@ export default function Projects() {
                                         href={item.siteLink}
                                         target="_blank"
                                         rel="noopener noreferrer"
-                                        className="text-blue-500"
+                                        className="text-blue-500 hover:underline"
                                     >
                                         Website
                                     </a>
