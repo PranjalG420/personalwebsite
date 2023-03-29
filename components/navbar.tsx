0c1c4bf7fe940c3421406b53fcbf085a6bc1421a
Pranjal Gupta <95481637+PranjalG420@users.noreply.github.com>
2023-03-29 20:09:51 +0530

    style: structure/colors of navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,29 +1,24 @@
-export default function navbar() {
+export default function Navbar() {
     return (
-        <nav className="flex flex-row sticky top-0 w-full justify-between p-2 bg-none desktop:text-xl text-base backdrop-blur print:hidden">
-            <div className="flex items-center flex-row gap-2">
-                <a
-                    href="#"
-                    className="text-white w-full bg-gradient-to-r from-blue-500 via-blue-600 to-indigo-600 hover:bg-gradient-to-br focus:outline-none shadow-sm shadow-blue-800/80 font-semibold rounded text-xl px-4 py-2 text-center"
-                >
-                    Home
-                </a>
-                <a
-                    href="#about"
-                    className="px-4 py-2 rounded hover:bg-emerald-700 transition-colors font-semibold"
-                >
-                    About
-                </a>
-                <a
-                    href="#projects"
-                    className="px-4 py-2 rounded hover:bg-emerald-700 transition-colors font-semibold"
-                >
-                    Projects
-                </a>
-            </div>
-            <button className="px-4 py-2 rounded hover:bg-emerald-700 transition-colors font-semibold">
-                Resume
-            </button>
+        <nav className="h-[8vh] flex flex-row justify-start gap-2 desktop:text-lg text-base items-center bg-none backdrop-blur-sm py-2 sticky top-0 left-0 w-full z-50">
+            <a
+                href="/#"
+                className="px-2 py-1 hover:bg-neutral-700 transition-colors rounded"
+            >
+                Home
+            </a>
+            <a
+                href="#about"
+                className="px-2 py-1 hover:bg-neutral-700 transition-colors rounded"
+            >
+                About
+            </a>
+            <a
+                href="#projects"
+                className="px-2 py-1 hover:bg-neutral-700 transition-colors rounded"
+            >
+                Projects
+            </a>
         </nav>
     );
 }
