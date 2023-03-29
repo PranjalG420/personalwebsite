84f0147cb856f0a388399d54b5124e2e01aa1886
Pranjal Gupta <95481637+PranjalG420@users.noreply.github.com>
2023-03-29 10:37:40 +0530

    fix: updated imports for styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,9 @@
 
 module.exports = {
     content: [
-        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./src/app/**/*.{js,ts,jsx,tsx}",
         "./components/**/*.{js,ts,jsx,tsx}",
+        "./src/pages/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
         screens: {
