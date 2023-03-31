01439cfbb07ed8196c844a9c77127e0b7797b954
Pranjal Gupta <95481637+PranjalG420@users.noreply.github.com>
2023-03-31 10:59:09 +0530

    style: removed rounded

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,19 +3,19 @@ export default function Navbar() {
         <nav className="h-[8vh] flex flex-row justify-start gap-2 desktop:text-lg text-base items-center bg-none backdrop-blur-sm py-2 sticky top-0 left-0 w-full z-50">
             <a
                 href="/#"
-                className="px-2 py-1 hover:bg-neutral-700 transition-colors rounded"
+                className="px-2 py-1 hover:bg-neutral-700 transition-colors"
             >
                 Home
             </a>
             <a
                 href="#about"
-                className="px-2 py-1 hover:bg-neutral-700 transition-colors rounded"
+                className="px-2 py-1 hover:bg-neutral-700 transition-colors"
             >
                 About
             </a>
             <a
                 href="#projects"
-                className="px-2 py-1 hover:bg-neutral-700 transition-colors rounded"
+                className="px-2 py-1 hover:bg-neutral-700 transition-colors"
             >
                 Projects
             </a>
