c43977d44edb873e32fb74bea5c485847cb9146a
Pranjal Gupta <pranjalg1122@gmail.com>
2023-11-30 23:16:34 +0530

    fix: home page profile picture

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -45,7 +45,7 @@ export default function Home() {
             <div className="min-h-[92vh] w-full gap-6 flex flex-col justify-center">
                 <div className="flex sm:flex-row flex-col gap-4 sm:items-center items-start justify-center">
                     <img
-                        src="https://github.com/pranjalg420.png"
+                        src="https://github.com/pranjalg1122.png"
                         className="rounded-full desktop:h-40 h-28"
                     />
                     <div className="flex flex-col sm:gap-1">
