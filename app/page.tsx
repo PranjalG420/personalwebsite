911989f1b0c5fca1f2cdad47e723e86570c550be
PranjalG1122 <pranjalg1122@gmail.com>
2024-06-18 21:46:43 +0530

    style: navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
+import Navbar from "@/components/Navbar";
 import Image from "next/image";
 
 export default function Home() {
-  return <main>home</main>;
+  return (
+    <main className="flex flex-col items-center w-screen min-h-screen flex-1">
+      <div className="flex flex-col items-start w-full max-w-3xl">
+        <Navbar />
+      </div>
+    </main>
+  );
 }
