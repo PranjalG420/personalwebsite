9080ed2e658ddc4e166a1acb9891e27538dda590
PranjalG1122 <pranjalg1122@gmail.com>
2024-07-05 02:25:37 +0530

    feat: text and resume

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,16 +1,22 @@
 import { variants } from "@/components/Button";
+import Container from "@/components/Container";
 import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <main className="flex flex-col items-center justify-center gap-4 w-full min-h-screen h-full">
-      <h1 className="text-4xl font-semibold">404</h1>
-      <p className="md:text-lg text-base text-neutral-400">
-        That page does not exist. 
-      </p>
-      <Link href="/" className={variants({ variant: "primary" }) + " max-w-fit"}>
-        Home
-      </Link>
-    </main>
+    <Container>
+      <div className="w-full flex flex-col items-center gap-2">
+        <h1 className="text-4xl font-semibold">404</h1>
+        <p className="md:text-lg text-base text-neutral-400">
+          That page does not exist.
+        </p>
+        <Link
+          href="/"
+          className={variants({ variant: "primary" }) + " max-w-fit"}
+        >
+          Home
+        </Link>
+      </div>
+    </Container>
   );
 }
